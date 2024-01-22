6902ca03fadeef8a4524f3cd412fcf606cd7e077
Suseenthiran Arulraj Rumeasiyan <srumeasiyan@gmail.com>
2024-01-22 23:14:47 +0530

    adds code to background.js

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.tabs.onUpdated.addListener(function (tabId, tab) {
+    if (tab.url && tab.url.includes("youtube.com/watch")) {
+        const queryParam = tab.url.split("?")[1];
+        const urlParams = new URLSearchParams(queryParam);
+
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            videoId: urlParams.get("v"),
+        });
+    }
+});
