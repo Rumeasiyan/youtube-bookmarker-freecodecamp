e5368ce14cfee5928efe29c32dbdd4ca0f1d50ec
Suseenthiran Arulraj Rumeasiyan <srumeasiyan@gmail.com>
2024-01-23 11:33:49 +0530

    updates background.js

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-chrome.tabs.onUpdated.addListener(function (tabId, tab) {
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
         const queryParam = tab.url.split("?")[1];
         const urlParams = new URLSearchParams(queryParam);
-        console.log(urlParams.get("v"));
 
         chrome.tabs.sendMessage(tabId, {
             type: "NEW",
