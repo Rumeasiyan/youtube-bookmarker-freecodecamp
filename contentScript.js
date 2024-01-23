aa37a55a5c2c3798033094e7b8cbcf3a517c983c
Suseenthiran Arulraj Rumeasiyan <srumeasiyan@gmail.com>
2024-01-23 11:59:09 +0530

    updates contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
-() => {
+(() => {
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
+    let currentVideoBookmarks = [];
 
     chrome.runtime.onMessage.addListener((obj, sender, respones) => {
         const { type, value, videoId } = obj;
@@ -11,9 +12,18 @@
         }
     });
 
-    const newVideoLoaded = () => {
+    const fetchBookmarks = () => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+            });
+        });
+    };
+
+    const newVideoLoaded = async () => {
         const bookmarkBtnExists =
             document.getElementsByClassName("bookmark-btn")[0];
+        currentVideoBookmarks = await fetchBookmarks();
 
         if (!bookmarkBtnExists) {
             const bookmarkBtn = document.createElement("img");
@@ -27,8 +37,34 @@
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
 
             youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
         }
     };
 
+    const addNewBookmarkEventHandler = async () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            timestamp: currentTime,
+            desc: "Bookmark at " + getCurrentTime(currentTime),
+        };
+
+       currentVideoBookmarks = await fetchBookmarks();
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify(
+                [...currentVideoBookmarks, newBookmark].sort(
+                    (a, b) => a.timestamp - b.timestamp
+                )
+            ),
+        });
+    };
+
     newVideoLoaded();
+})();
+
+const getCurrentTime = (time) => {
+    var date = new Date(0);
+    date.setSeconds(time);
+
+    return date.toISOString().substr(11, 8);
 };
